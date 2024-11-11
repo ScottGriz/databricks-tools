f7d80ea8197f82f2a151822364b4593be8bd405b
Scott Griz <scottgriz@gmail.com>
2024-11-11 15:37:15 -0800

    Added support for Private DBFS

---
+++ adb-vnet-injection-backend-pl-uc/dbfs.tf
@@ -0,0 +1,77 @@
+locals {
+  dbfs_resource_id = "${azurerm_databricks_workspace.this.managed_resource_group_id}/providers/Microsoft.Storage/storageAccounts/${azurerm_databricks_workspace.this.custom_parameters[0].storage_account_name}"
+}
+
+# Access Connector needed for Private DBFS
+resource "azurerm_databricks_access_connector" "dbfs" {
+  name                = "${var.prefix}-databricks-mi"
+  #Does this have to go in the MRG???
+  resource_group_name = azurerm_resource_group.resourcegroup.name
+  location            = var.location
+  identity {
+    type = "SystemAssigned"
+  }
+  tags = local.tags
+}
+
+resource "azurerm_private_endpoint" "dfspe" {
+  name                = "dbfs-dfs-pe"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.resourcegroup.name
+  subnet_id           = azurerm_subnet.plsubnet.id
+
+  private_dns_zone_group {
+    name = "add_to_azure_private_dns_dfs"
+    private_dns_zone_ids = [azurerm_private_dns_zone.adlsstorage.id]
+  }
+
+  private_service_connection {
+    name                           = "${var.prefix}-pl-dfs"
+    private_connection_resource_id = local.dbfs_resource_id
+    subresource_names              = ["dfs"]
+    is_manual_connection = false
+  }
+}
+
+resource "azurerm_private_endpoint" "blobpe" {
+  name                = "dbfs-blob-pe"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.resourcegroup.name
+  subnet_id           = azurerm_subnet.plsubnet.id
+  
+    private_dns_zone_group {
+    name = "add_to_azure_private_dns_blob"
+    private_dns_zone_ids = [azurerm_private_dns_zone.blobstorage.id]
+  }
+
+  private_service_connection {
+    name                           = "${var.prefix}-pl-blob"
+    private_connection_resource_id = local.dbfs_resource_id
+    subresource_names              = ["blob"]
+    is_manual_connection = false
+  }
+}
+
+resource "azurerm_private_dns_zone" "blobstorage" {
+  name                = "privatelink.blob.core.windows.net"
+  resource_group_name = azurerm_resource_group.resourcegroup.name
+}
+
+resource "azurerm_private_dns_zone" "adlsstorage" {
+  name                = "privatelink.dfs.core.windows.net"
+  resource_group_name = azurerm_resource_group.resourcegroup.name
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "dfszone" {
+  name                  = "${var.prefix}-dfslink"
+  resource_group_name   = azurerm_resource_group.resourcegroup.name
+  private_dns_zone_name = azurerm_private_dns_zone.adlsstorage.name
+  virtual_network_id    = azurerm_virtual_network.vnet_for_databricks.id
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "blobzone" {
+  name                  = "${var.prefix}-bloblink"
+  resource_group_name   = azurerm_resource_group.resourcegroup.name
+  private_dns_zone_name = azurerm_private_dns_zone.blobstorage.name
+  virtual_network_id    = azurerm_virtual_network.vnet_for_databricks.id
+}
--- adb-vnet-injection-backend-pl-uc/versions.tf
+++ adb-vnet-injection-backend-pl-uc/versions.tf
@@ -1,16 +1,12 @@
-
+# versions.tf
 terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">=3.66.0"
+      version = "~>3.104.0"
     }
-
     random = {
       source  = "hashicorp/random"
-      version = ">=3.4.1"
     }
-
   }
-
 }
